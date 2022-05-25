fa26c22ab005f5c6dc629cdf0a8378c3c00ad787
George B <george@wizardofweb.com>
2022-05-25 11:51:12 +0800

    added our sign in component as well as firebase utils and brought in firebase libraries

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/Navigation';
 import './App.css';
-import Directory from './components/category-item/directory/Directory';
+import Home from './routes/home/Home';
+import SignIn from './routes/sign-in/SignIn';
 
-const categories = [
-  {
-    id: 1,
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-  },
-  {
-    id: 2,
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-  },
-  {
-    id: 3,
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-  },
-  {
-    id: 4,
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-  },
-  {
-    id: 5,
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-  },
-];
+const Hello = () => {
+  return (
+    <div>
+      <div>
+        <h1>Hello there</h1>
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Hello />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/sign-in/SignIn.js
@@ -0,0 +1,20 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAut,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase.utils.js';
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+    </div>
+  );
+};
+
+export default SignIn;
